5dd306d50c2566b30c11a05458225fea2aebe5b1
[dnqls2446] <[dnqls9875@gmail.com]>
2024-02-21 17:52:30 +0900

    스터디 2일차

--- js_video/20.study/app.js
+++ js_video/20.study/app.js
@@ -1,18 +1,34 @@
 const input = document.querySelector(".input");
-const button = document.querySelector(".console-btn");
+const closeBtn = document.querySelector(".console-btn");
 const form = document.querySelector(".form");
+const ul = document.querySelector("ul");
 
-const active = [input, button];
+const active = [input, closeBtn];
 
-// console 출력 버튼 입력 시 console창 내용 입력
-form.addEventListener("submit", (event) => {
+// 삭제 버튼 클릭 시
+const delItem = (event) => {
+  const target = event.target.parentElement;
+  target.remove();
+};
+
+const addItem = (event) => {
   // 새로고침 방지 코드
   event.preventDefault();
 
   // input.value 가 공백이 아닐 시
   if (input.value !== "") {
-    console.log(input.value);
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    const span = document.createElement("span");
+
+    span.innerHTML = input.value;
+    button.innerText = "삭제";
+    button.addEventListener("click", delItem);
     input.value = "";
+
+    li.appendChild(span);
+    li.appendChild(button);
+    ul.appendChild(li);
   } else {
     console.log("내용을 입력하세요");
   }
@@ -21,4 +37,6 @@ form.addEventListener("submit", (event) => {
   active.forEach((element) => {
     element.classList.add("active");
   });
-});
+};
+
+form.addEventListener("submit", addItem);
