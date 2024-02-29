3724a621b5cfca36a1fc3ec1dfad63df9b57ba7a
[dnqls2446] <[dnqls9875@gmail.com]>
2024-02-29 17:20:13 +0900

    table연습

--- js_video/20.study1/practice.js
+++ js_video/20.study1/practice.js
@@ -1,31 +1,39 @@
-// count
-const h1 = document.querySelector("h1");
-const button = document.querySelector("button");
-const ul = document.querySelector("ul");
+// // count
+// const h1 = document.querySelector("h1");
+// const button = document.querySelector("button");
+// const ul = document.querySelector("ul");
 
-let count = 0;
+// let count = 0;
 
-button.addEventListener("click", function () {
-  count += 100;
-  h1.innerHTML = count;
-});
+// button.addEventListener("click", function () {
+//   count += 100;
+//   h1.innerHTML = count;
+// });
 
-// 배열 1-50까지 돌리기
-const num = 50;
+// // 배열 1-50까지 돌리기
+// const num = 50;
 
-for (let i = 1; i <= num; i++) {
-  const li = document.createElement("li");
-  li.innerHTML = `<p>${i}</p>`;
-  ul.appendChild(li);
-}
+// for (let i = 1; i <= num; i++) {
+//   const li = document.createElement("li");
+//   li.innerHTML = `<p>${i}</p>`;
+//   ul.appendChild(li);
+// }
 
-// 배열 1-50까지 돌리기
-const array = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-  42, 43, 44, 45, 46, 47, 48, 49, 50,
-];
+// // 배열 1-50까지 돌리기
+// const array = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+//   23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+//   42, 43, 44, 45, 46, 47, 48, 49, 50,
+// ];
 
-array.forEach((element) => {
-  console.log(element);
-});
+// array.forEach((element) => {
+//   console.log(element);
+// });
+
+var string = "hello";
+string.split("");
+
+var string2 = "cat,dog,pig";
+string2.split("");
+
+console.log(string2);
